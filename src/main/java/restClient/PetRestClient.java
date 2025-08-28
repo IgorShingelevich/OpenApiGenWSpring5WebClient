d86052a8264d835bf62cebfcc7b24d0e175a5c72
shingelevich <63087390+poploveok@users.noreply.github.com>
2025-08-28 21:15:07 +0300

    Remove WebClientConfig.java file

---
+++ src/main/java/restClient/PetRestClient.java
@@ -0,0 +1,5 @@
+package restClient;
+
+public class PetRestClient {
+    //crud operations methods to fill dto
+}
---
+++ src/main/java/restClient/UserRestClient.java
@@ -0,0 +1,5 @@
+package restClient;
+
+public class UserRestClient {
+        //crud operations methods to fill dto
+}
